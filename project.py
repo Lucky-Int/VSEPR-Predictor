c135207f01ff84cb5e068abe512c58a57c0cc7f4
Lucky-Int <luckyshayare@gmail.com>
2026-08-18 17:34:40 -0400

    Find total Valence electrons and central atom.
    Added mendeleev library for elemental data like electronegativity and valence electrons.

--- project.py
+++ project.py
@@ -1,4 +1,6 @@
 # VSEPR Predictor and Modeler
+# rename property so variable names don't collide
+from mendeleev import element as get_element
 import re
 molecule = input("Molecule: ")
 # ERROR CHECK ^^^
@@ -12,36 +14,49 @@ molecule = input("Molecule: ")
 
 # check the coefficient of the molecule
 if matches := re.search(r"^(\d)", molecule):
-    coefficient = matches.group(1)
+    coefficient = int(matches.group(1))
     no_c_molecule = molecule[1:]
 else:
-    coefficient = "1"
+    coefficient = 1
     no_c_molecule = molecule
 
-elements = []
-subscripts = []
-for a in no_c_molecule:
-    if a.isalpha():
-        elements.append(a)
-
-    else:
-        subscripts.append(a)
 
 matches = re.findall(r"([A-Z][a-z]?)(\d*)" ,no_c_molecule)
-
+elements = []
 elements_and_nums = []
+ve_total = 0
 for element, subscript in matches:
     if subscript == "":
         subscript = "1"
-
+    elements.append(element)
     element_w_num = {
          "Element": element,
          "Subscript": subscript,
          }
     elements_and_nums.append(element_w_num)
 
-print(elements_and_nums)
+    # calculate valence electron total
+    ve = get_element(element).nvalence()
+    ve_part = ve * int(subscript)
+    ve_total += ve_part
+# ACCOUNT FOR OVERALL CHARGE (POLYATOMIC ATOMS) URGENCY: !!!
+# find element with lowest electronegativity (central atom)
+electronegativities = []
+no_h_elements = [e for e in elements if e != "H"]
+for element in no_h_elements:
+        element = get_element(element)
+        electronegativities.append(element.electronegativity())
 
+lowest_eneg = min(electronegativities)
+position = electronegativities.index(lowest_eneg)
+central_atom = no_h_elements[position]
+
+
+print(elements_and_nums)
+print(f"Valence Electrons: {ve_total}")
+print(f"Central Atom: {central_atom}")
+
+# ex: [{'Element': 'H', 'Subscript': '2'}, {'Element': 'O', 'Subscript': '1'}]
 
 
     # Consider 2 electrons used for each bond shared between the core atom and another one.
