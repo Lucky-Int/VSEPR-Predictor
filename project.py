9fb6885e6f1bc43ad391f01cbe6c4a05ec7c6c8e
Lucky-Int <luckyshayare@gmail.com>
2026-08-19 20:44:13 -0400

    Accounted for polyatomic atoms (charges) and error checked
    Error checked/parsed user input and accounted charges for total Valence Electrons.

--- project.py
+++ project.py
@@ -1,60 +1,105 @@
+# Next up --> Clean up code to create functions like get_valence_total() and get_central_atom()
+# Next up --> start creating abstract lewis structure: central atom to terminal atoms covalent bonds. 2 V.E each.
+
+
 # VSEPR Predictor and Modeler
 # rename property so variable names don't collide
 from mendeleev import element as get_element
 import re
-molecule = input("Molecule: ")
-# ERROR CHECK ^^^
-# ex input: CH3OH
-# Create a molecular diagram; use RDKit.
-# convert molecule into necessary type.
+import sys
+from chemicals import search_chemical
 
-# draw diagram of molecule
-
-    # Count the number of Valence Electrons in the compound (consider the charge as well)
-
-# check the coefficient of the molecule
-if matches := re.search(r"^(\d)", molecule):
-    coefficient = int(matches.group(1))
-    no_c_molecule = molecule[1:]
-else:
-    coefficient = 1
-    no_c_molecule = molecule
+def main():
+    molecule = input("Molecule: ")
+    if not validate_formula(molecule):
+        sys.exit("Not a valid molecule")
 
 
-matches = re.findall(r"([A-Z][a-z]?)(\d*)" ,no_c_molecule)
-elements = []
-elements_and_nums = []
-ve_total = 0
-for element, subscript in matches:
-    if subscript == "":
-        subscript = "1"
-    elements.append(element)
-    element_w_num = {
-         "Element": element,
-         "Subscript": subscript,
-         }
-    elements_and_nums.append(element_w_num)
-
-    # calculate valence electron total
-    ve = get_element(element).nvalence()
-    ve_part = ve * int(subscript)
-    ve_total += ve_part
-# ACCOUNT FOR OVERALL CHARGE (POLYATOMIC ATOMS) URGENCY: !!!
-# find element with lowest electronegativity (central atom)
-electronegativities = []
-no_h_elements = [e for e in elements if e != "H"]
-for element in no_h_elements:
-        element = get_element(element)
-        electronegativities.append(element.electronegativity())
-
-lowest_eneg = min(electronegativities)
-position = electronegativities.index(lowest_eneg)
-central_atom = no_h_elements[position]
+    me = range(1)
 
 
-print(elements_and_nums)
-print(f"Valence Electrons: {ve_total}")
-print(f"Central Atom: {central_atom}")
+    for motivation in me:
+        lock_in(motivation)
+
+
+
+
+    for _ in range(120):
+        print("-", end = "")
+    print()
+
+    # Count the number of Valence Electrons in the compound-- consider the charge as well.
+
+
+    if matches := re.search(r"^(\d)", molecule):
+        coefficient = int(matches.group(1))
+        no_c_molecule = molecule[1:]
+    else:
+        coefficient = 1
+        no_c_molecule = molecule
+    # Maybe uneccesary to find coefficient ^^^
+    matches = re.findall(r"([A-Z][a-z]?)(\d?)" ,no_c_molecule)
+    elements = []
+    elements_and_nums = []
+    pure_ve_total = 0
+    for element, subscript, in matches:
+        if subscript == "":
+            subscript = "1"
+        elements.append(element)
+        element_w_num = {
+            "Element": element,
+            "Subscript": subscript,
+            }
+        elements_and_nums.append(element_w_num)
+
+        # calculate valence electron total
+        ve = get_element(element).nvalence()
+        ve_part = ve * int(subscript)
+        pure_ve_total += ve_part
+    if matches := re.search(r"(\d)?(\+|-)$", molecule):
+        if matches.group(1) is None:
+            charge_mag = 1
+        else:
+            charge_mag = int(matches.group(1))
+        charge_sign = matches.group(2)
+        if charge_sign == "+":
+            ve_total = pure_ve_total - charge_mag
+        else:
+            ve_total = pure_ve_total + charge_mag
+    else:
+        ve_total = pure_ve_total
+    # find element with lowest electronegativity (central atom, excluding hydrogen)
+    electronegativities = []
+    no_h_elements = [e for e in elements if e != "H"]
+    for element in no_h_elements:
+            element = get_element(element)
+            electronegativities.append(element.electronegativity())
+
+    lowest_eneg = min(electronegativities)
+    position = electronegativities.index(lowest_eneg)
+    central_atom = no_h_elements[position]
+
+
+    print(elements_and_nums)
+    print(f"Valence Electrons: {ve_total}")
+    print(f"Central Atom: {central_atom}")
+
+
+def get_central_atom():
+    ...
+
+def get_total_valence():
+    ...
+def lock_in(m):
+    return None
+
+def validate_formula(formula):
+    try:
+        result = search_chemical(formula)
+        return True
+    except ValueError:
+        return False
+
 
 # ex: [{'Element': 'H', 'Subscript': '2'}, {'Element': 'O', 'Subscript': '1'}]
 
@@ -67,10 +112,11 @@ print(f"Central Atom: {central_atom}")
 
     # If the central atom does not have enough electrons, needing at least 8, create more covalent bonds.
 
-    # Check formal charges and if they can be reduced closer 0 do as that. """
+    # Check formal charges and if they can be reduced closer 0 do as that.
 
 
 
 # Count the amount of electron pairs/clouds around the central atom; give the compounds its electron-group geo.
 # ignore lone pairs and consider the greater space they take up to predict the molecular geometry.
-
+if __name__ == "__main__":
+    main()
