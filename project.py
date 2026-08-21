afc29a0cc5151da95746b17f4a0114f10ee44359
Lucky-Int <luckyshayare@gmail.com>
2026-08-21 18:03:23 -0400

    Created organized functions.

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 # Next up --> Clean up code to create functions like get_valence_total() and get_central_atom()
 # Next up --> start creating abstract lewis structure: central atom to terminal atoms covalent bonds. 2 V.E each.
 
-
+# FIX +- CHARGE LOGIC; SUBSCRIPTS COLLIDING. URGENCY: !!!
 # VSEPR Predictor and Modeler
 # rename property so variable names don't collide
 from mendeleev import element as get_element
@@ -10,10 +10,10 @@ import sys
 from chemicals import search_chemical
 
 def main():
-    molecule = input("Molecule: ")
-    if not validate_formula(molecule):
+    raw_input = input("Molecule: ")
+    if not validate_formula(raw_input):
         sys.exit("Not a valid molecule")
-
+    molecule = raw_input
 
     me = range(1)
 
@@ -38,46 +38,9 @@ def main():
         coefficient = 1
         no_c_molecule = molecule
     # Maybe uneccesary to find coefficient ^^^
-    matches = re.findall(r"([A-Z][a-z]?)(\d?)" ,no_c_molecule)
-    elements = []
-    elements_and_nums = []
-    pure_ve_total = 0
-    for element, subscript, in matches:
-        if subscript == "":
-            subscript = "1"
-        elements.append(element)
-        element_w_num = {
-            "Element": element,
-            "Subscript": subscript,
-            }
-        elements_and_nums.append(element_w_num)
-
-        # calculate valence electron total
-        ve = get_element(element).nvalence()
-        ve_part = ve * int(subscript)
-        pure_ve_total += ve_part
-    if matches := re.search(r"(\d)?(\+|-)$", molecule):
-        if matches.group(1) is None:
-            charge_mag = 1
-        else:
-            charge_mag = int(matches.group(1))
-        charge_sign = matches.group(2)
-        if charge_sign == "+":
-            ve_total = pure_ve_total - charge_mag
-        else:
-            ve_total = pure_ve_total + charge_mag
-    else:
-        ve_total = pure_ve_total
-    # find element with lowest electronegativity (central atom, excluding hydrogen)
-    electronegativities = []
-    no_h_elements = [e for e in elements if e != "H"]
-    for element in no_h_elements:
-            element = get_element(element)
-            electronegativities.append(element.electronegativity())
-
-    lowest_eneg = min(electronegativities)
-    position = electronegativities.index(lowest_eneg)
-    central_atom = no_h_elements[position]
+    elements_and_nums = get_atoms_subscripts(no_c_molecule)
+    ve_total = get_total_valence(no_c_molecule)
+    central_atom = get_central_atom(no_c_molecule)
 
 
     print(elements_and_nums)
@@ -85,11 +48,71 @@ def main():
     print(f"Central Atom: {central_atom}")
 
 
-def get_central_atom():
-    ...
+def get_central_atom(molecule):
+    matches = re.findall(r"([A-Z][a-z]?)(\d?)" ,molecule)
 
-def get_total_valence():
-    ...
+    elements = [atom for atom, _, in matches if atom]
+
+
+
+    electronegativities = []
+    no_h_elements = [e for e in elements if e != "H"]
+    for element in no_h_elements:
+            element_obj = get_element(element)
+            electronegativities.append(element_obj.electronegativity())
+
+    lowest_eneg = min(electronegativities)
+    position = electronegativities.index(lowest_eneg)
+    central_atom = no_h_elements[position]
+    return central_atom
+
+def get_total_valence(molecule):
+    pure_ve_total = 0
+    atom_matches = re.findall(r"([A-Z][a-z]?)(\d?)" ,molecule)
+    for element, subscript, in atom_matches:
+        if not element:
+            continue
+
+
+
+        ve = get_element(element).nvalence()
+        try:
+            ve_part = ve * int(subscript)
+        except ValueError:
+            ve_part = ve * 1
+        pure_ve_total += ve_part
+    if charge_matches := re.search(r"(\d)?(\+|-)$", molecule):
+            if charge_matches.group(1) is None:
+                charge_mag = 1
+            else:
+                charge_mag = int(charge_matches.group(1))
+            charge_sign = charge_matches.group(2)
+            if charge_sign == "+":
+                ve_total = pure_ve_total - charge_mag
+            else:
+                ve_total = pure_ve_total + charge_mag
+    else:
+        ve_total = pure_ve_total
+    return ve_total
+
+def get_atoms_subscripts(molecule):
+
+    matches = re.findall(r"([A-Z][a-z]?)(\d?)" ,molecule)
+    elements_and_nums = []
+    for element, subscript, in matches:
+
+        if not element:
+            continue
+
+        if subscript == "":
+            subscript = "1"
+
+        element_w_num = {
+            "Element": element,
+            "Subscript": subscript,
+            }
+        elements_and_nums.append(element_w_num)
+    return elements_and_nums
 def lock_in(m):
     return None
 
