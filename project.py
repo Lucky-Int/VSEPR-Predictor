1ca98900364094d844473927810e33833fc7cdf5
Lucky-Int <luckyshayare@gmail.com>
2026-08-26 17:04:46 -0400

    Calculated Molecular and Electron Geometries, hybridization, and bond angles.
    Used formal VSEPR Formula and a dictionary containing all information.

--- project.py
+++ project.py
@@ -1,6 +1,5 @@
-# Next up --> Clean up code to create functions like get_valence_total() and get_central_atom()
-# Next up --> start creating abstract lewis structure: central atom to terminal atoms covalent bonds. 2 V.E each.
 
+# Try writing Double and Triple bond code. And create the final lewis structure.
 # VSEPR Predictor and Modeler
 # rename property so variable names don't collide
 from mendeleev import element as get_element
@@ -23,9 +22,8 @@ def main():
 
 
 
-    for _ in range(120):
-        print("-", end = "")
-    print()
+
+    print("-" * 120)
 
     # Count the number of Valence Electrons in the compound-- consider the charge as well.
 
@@ -62,27 +60,165 @@ def main():
     # add lone pairs if needed
     if track_ve != 0:
         electron_groups = num_terminal_atoms
-        central_lone_pairs = track_ve / 2
+        central_lone_pairs = int(track_ve / 2)
         electron_groups += central_lone_pairs
         track_ve = 0
         central_electrons = central_lone_pairs * 2 + num_terminal_atoms
-
     # check formal charge of central atom
         central_atom_valence = get_element(central_atom).nvalence()
-        formal_charge = central_atom_valence - central_electrons
+        central_formal_charge = central_atom_valence - central_electrons
     else:
         electron_groups = num_terminal_atoms
         central_electrons = num_terminal_atoms
-        formal_charge = get_element(central_atom).nvalence() - central_electrons
-    # go on with adding double or triple bonds. (If input requires).
+        central_formal_charge = get_element(central_atom).nvalence() - central_electrons
+        central_lone_pairs = 0
+
+    # go on with adding double or triple bonds. (If formal_charge_central != 0).
+
+
     try:
         print("Lone Pairs:", central_lone_pairs)
     except UnboundLocalError:
         pass
-    print("Electron Groups:", electron_groups)
+    print("Electron Groups / Steric Number:", electron_groups)
     print("Terminal Atoms:", terminal_atoms)
-    print("VE:", track_ve)
-    print("Formal Charge:", formal_charge)
+
+
+
+
+    vsepr_table = {
+    "AX2": {
+        "steric_number": 2,
+        "bonding_groups": 2,
+        "lone_pairs": 0,
+        "electron_geometry": "Linear",
+        "molecular_geometry": "Linear",
+        "bond_angles": "180",
+        "hybridization": "sp"
+    },
+    "AX3": {
+        "steric_number": 3,
+        "bonding_groups": 3,
+        "lone_pairs": 0,
+        "electron_geometry": "Trigonal Planar",
+        "molecular_geometry": "Trigonal Planar",
+        "bond_angles": "120",
+        "hybridization": "sp2"
+    },
+    "AX2E": {
+        "steric_number": 3,
+        "bonding_groups": 2,
+        "lone_pairs": 1,
+        "electron_geometry": "Trigonal Planar",
+        "molecular_geometry": "Bent",
+        "bond_angles": "<120",
+        "hybridization": "sp2"
+    },
+    "AX4": {
+        "steric_number": 4,
+        "bonding_groups": 4,
+        "lone_pairs": 0,
+        "electron_geometry": "Tetrahedral",
+        "molecular_geometry": "Tetrahedral",
+        "bond_angles": "109.5",
+        "hybridization": "sp3"
+    },
+    "AX3E": {
+        "steric_number": 4,
+        "bonding_groups": 3,
+        "lone_pairs": 1,
+        "electron_geometry": "Tetrahedral",
+        "molecular_geometry": "Trigonal Pyramidal",
+        "bond_angles": "~107",
+        "hybridization": "sp3"
+    },
+    "AX2E2": {
+        "steric_number": 4,
+        "bonding_groups": 2,
+        "lone_pairs": 2,
+        "electron_geometry": "Tetrahedral",
+        "molecular_geometry": "Bent",
+        "bond_angles": "~104.5",
+        "hybridization": "sp3"
+    },
+    "AX5": {
+        "steric_number": 5,
+        "bonding_groups": 5,
+        "lone_pairs": 0,
+        "electron_geometry": "Trigonal Bipyramidal",
+        "molecular_geometry": "Trigonal Bipyramidal",
+        "bond_angles": "90, 120, 180",
+        "hybridization": "sp3d"
+    },
+    "AX4E": {
+        "steric_number": 5,
+        "bonding_groups": 4,
+        "lone_pairs": 1,
+        "electron_geometry": "Trigonal Bipyramidal",
+        "molecular_geometry": "Seesaw",
+        "bond_angles": "<90, <120",
+        "hybridization": "sp3d"
+    },
+    "AX3E2": {
+        "steric_number": 5,
+        "bonding_groups": 3,
+        "lone_pairs": 2,
+        "electron_geometry": "Trigonal Bipyramidal",
+        "molecular_geometry": "T-Shaped",
+        "bond_angles": "<90",
+        "hybridization": "sp3d"
+    },
+    "AX2E3": {
+        "steric_number": 5,
+        "bonding_groups": 2,
+        "lone_pairs": 3,
+        "electron_geometry": "Trigonal Bipyramidal",
+        "molecular_geometry": "Linear",
+        "bond_angles": "180",
+        "hybridization": "sp3d"
+    },
+    "AX6": {
+        "steric_number": 6,
+        "bonding_groups": 6,
+        "lone_pairs": 0,
+        "electron_geometry": "Octahedral",
+        "molecular_geometry": "Octahedral",
+        "bond_angles": "90, 180",
+        "hybridization": "sp3d2"
+    },
+    "AX5E": {
+        "steric_number": 6,
+        "bonding_groups": 5,
+        "lone_pairs": 1,
+        "electron_geometry": "Octahedral",
+        "molecular_geometry": "Square Pyramidal",
+        "bond_angles": "<90",
+        "hybridization": "sp3d2"
+    },
+    "AX4E2": {
+        "steric_number": 6,
+        "bonding_groups": 4,
+        "lone_pairs": 2,
+        "electron_geometry": "Octahedral",
+        "molecular_geometry": "Square Planar",
+        "bond_angles": "90",
+        "hybridization": "sp3d2"
+    }
+}
+    if central_lone_pairs != 0:
+        vsepr_form = f"AX{num_terminal_atoms}E{central_lone_pairs}"
+    else:
+        vsepr_form = f"AX{num_terminal_atoms}"
+    e_geo = vsepr_table[vsepr_form]["electron_geometry"]
+    mol_geo = vsepr_table[vsepr_form]["molecular_geometry"]
+    hybridization = vsepr_table[vsepr_form]["hybridization"]
+    bond_angles = vsepr_table[vsepr_form]["bond_angles"]
+
+    print("Electron Geometry:", e_geo)
+    print("Molecular Geometry:", mol_geo)
+    print("Bond Hybridization:", hybridization)
+    print("Bond Angle(s):", bond_angles)
+
     # [{'Element': 'H', 'Subscript': '2'}, {'Element': 'O', 'Subscript': '1'}]
    # Consider 2 electrons used for each bond shared between the core atom and another one.
 
